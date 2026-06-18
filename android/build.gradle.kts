3d3f4efd6e06226462c258419da22768894c3aba
swangai7178 <swangai7178@gmail.com>
2026-06-18 09:38:10 +0300

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,8 +15,26 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.library") || project.plugins.hasPlugin("com.android.application")) {
+            val androidExtension = project.extensions.findByName("android")
+            if (androidExtension != null) {
+                val method = androidExtension.javaClass.getMethod("getNamespace")
+                val currentNamespace = method.invoke(androidExtension)
+                
+                if (currentNamespace == null) {
+                    val setNamespaceMethod = androidExtension.javaClass.getMethod("setNamespace", String::class.java)
+                    setNamespaceMethod.invoke(androidExtension, project.group.toString())
+                }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
